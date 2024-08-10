15348e733f32d090c98ad6694ce334a293fe7de6
Gustavo Carmo <gustavo.silva.carmo@hotmail.com>
2024-08-10 19:24:22 +0100

    adding zod to validate the .env file

---
+++ node/02-api-rest-nodejs/src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  PORT: z.preprocess((a) => parseInt(a as string), z.number()).default(3333),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('⚠️ Invalid envoriment variables!', _env.error.format())
+
+  throw new Error('⚠️ Invalid envoriment variables!')
+}
+
+console.log(_env.data)
+export const env = _env.data
